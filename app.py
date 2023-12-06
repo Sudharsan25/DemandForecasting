35332994b36d02d3f0acb8797b30ee58d533a351
Sudharsan <81094470+Sudharsan25@users.noreply.github.com>
2023-12-06 12:27:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pickle
 with open('catboost_model.pkl', 'rb') as file:
     catboost_regressor = pickle.load(file)
 
+with open('scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
+
 def preprocess(sales_price, base_price, featured, on_display, scaler):
     input_data = pd.DataFrame({
         'Sales_Price': [sales_price],
@@ -19,9 +22,7 @@ def preprocess(sales_price, base_price, featured, on_display, scaler):
     return input_data_scaled
 
 def home_page():
-    st.title("Demand Forecasting Application")
-    st.write(
-        """
+    st.write("""
         Welcome to the Demand Forecasting App! This app predicts the number of units sold based on input parameters.
         Use the navigation bar on the left to explore the app. 
 
@@ -30,25 +31,28 @@ def home_page():
         of need. This helps to avoid wastage, and at the same time meet the requirements of the consumer.
 
         To perform Demand forecasting, open source data from a particular store was used.
-        """
-    )
+    """)
 
 def predict_page():
-    st.title("Demand Forecasting")
     sales_price = st.number_input("Enter Sales Price:")
     base_price = st.number_input("Enter Base Price:")
     featured = st.checkbox("Featured Item of the Week (1 for Yes, 0 for No)")
     on_display = st.checkbox("Displayed Prominently (1 for Yes, 0 for No)")
 
     if st.button("Predict"):
-        with open('scaler.pkl', 'rb') as scaler_file:
-            scaler = pickle.load(scaler_file)
         input_data_scaled = preprocess(sales_price, base_price, featured, on_display, scaler)
         prediction = catboost_regressor.predict(input_data_scaled)
         st.success(f"Predicted Number of Units Sold: {prediction}")
 
 def main():
-    st.sidebar.title("Navigation")
+    st.set_page_config(
+        page_title="Demand Forecasting App",
+        page_icon=":chart_with_upwards_trend:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+
+    st.title("Demand Forecasting Application")
     pages = ["Home", "Predict"]
     selection = st.sidebar.radio("Go to", pages)
 
